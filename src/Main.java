f14464a6f71890432d32156cdd0a3dea84dac91e
User <xtoycohcohu@mail.ru>
2024-02-07 10:14:28 +0700

    Посмотрела решение в интернете.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Задание 4");
+        int deliveryDistance = 102;
+        int minDelivery = 1;
+        if (deliveryDistance > 100) {
+            System.out.println("Нет доставки");
+        } else {
+            if (deliveryDistance > 20) {
+                minDelivery++;
+            }
+            if (deliveryDistance > 60) {
+                minDelivery++;
+            }
+            System.out.println("Доставка займет " + minDelivery + " суток");
+        }
     }
 }
